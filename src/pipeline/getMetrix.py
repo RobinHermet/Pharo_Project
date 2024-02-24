092d5a41a5725871dc471c2e5874538d118584d2
etienne-tillier <etienne.tillier@etu.umontpellier.fr>
2024-02-24 17:07:56 -0500

    pipeline generalized only windows path lacking

--- src/pipeline/getMetrix.py
+++ src/pipeline/getMetrix.py
@@ -15,11 +15,12 @@ if len(sys.argv) != 2 or sys.argv[1] not in ['nestjs', 'loopbackjs']:
 project_type = sys.argv[1]
 
 # Configuration des chemins
+user_home_dir = os.path.expanduser('~')
 csv_projects_path = os.path.abspath('../../data/projects_' + project_type + '.csv')
 clone_path = os.path.abspath('../../cache/clones/' + project_type) + '/'
 moose_image_directory = '~/Documents/Pharo/images/Moose Suite 10 (stable)/'
 results_csv_path = os.path.abspath('../../data' + project_type + 'Results.csv')
-modelsDir = os.path.abspath('../../cache/modeles/' + project_type) + '/'
+modelsDir = os.path.abspath(user_home_dir + "/Documents/Pharo/images/Moose Suite 10 (stable)/modeles/" + project_type)
 projects = []
 
 # Créer le répertoire des clones s'il n'existe pas
@@ -38,6 +39,7 @@ def clone_repository(repo_url, clone_path, repo_name):
         print(f'Cloné: {repo_name}')
     else:
         print(f'Déjà cloné: {repo_name}')
+    os.chdir(os.path.join(os.path.dirname(__file__))) 
     return repo_clone_path
 
 def getModelFromTs2famix(repo_name):
@@ -48,28 +50,20 @@ def getModelFromTs2famix(repo_name):
     output_json = repo_name + "-model.json"
     if not os.path.exists(output_json):
         print(f"Exécution de ts2famix sur tsconfig.json, sortie dans {output_json}")
-        try:
-            subprocess.run(["ts2famix", "-i", "tsconfig.json", "-o", output_json], check=True, shell=True)
-        except subprocess.CalledProcessError as e:
-            print("error")
-            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+        # try:
+       # subprocess.run(["ts2famix", "-i", "tsconfig.json", "-o", output_json], check=True, shell=True)
+        subprocess.run(["ts2famix", "-i", "tsconfig.json", "-o", output_json], check=True )
+        # except subprocess.CalledProcessError as e:
+        #     print("error")
+        #     raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
     os.chdir(os.path.join(os.path.dirname(__file__)))
-
-# def createModeleFolderIfNot():
-#     os.chdir("../../../../../..")
-#     os.chdir("Documents")
-#     os.chdir("Pharo")
-#     os.chdir("images")
-#     os.chdir("Moose Suite 10 (stable)")
-#     if not os.path.exists("modeles"):
-#         os.makedirs('modeles', 755)
-#     os.chdir(os.path.join(os.path.dirname(__file__)))
 
 def createModeleFolderIfNot():
-    os.chdir(os.path.join(os.path.dirname(__file__)))
-    if not os.path.exists("../../cache/modeles/"):
-        os.makedirs('../../cache/modeles/', 755)
-    os.chdir('../../cache/modeles/')   
+    pharo_image_path = os.path.join(user_home_dir, 'Documents/Pharo/images/Moose Suite 10 (stable)')
+    os.chdir(pharo_image_path)
+    if not os.path.exists("modeles"):
+        os.makedirs('modeles', 755)
+    os.chdir("modeles")
     if not os.path.exists(project_type):
         os.makedirs(project_type, 755)
     os.chdir(os.path.join(os.path.dirname(__file__)))
@@ -78,12 +72,12 @@ def createModeleFolderIfNot():
 def copy_model_to_moose(repo_name):
     createModeleFolderIfNot()
     model_path = os.path.abspath(clone_path + repo_name + '/' + repo_name + '-model.json')  # Ajustez selon la structure de votre dossier
-    destination_path = os.path.expanduser(os.path.join(modelsDir, f'{repo_name}-model.json'))
+    destination_path = os.path.join(modelsDir, f'{repo_name}-model.json')
     if(SYSTEM=='Windows'):
         print("Copy with W")
         subprocess.run(['copy', model_path, destination_path], shell=True)
     else : 
-        subprocess.run(['cp', model_path, destination_path], shell=True)
+        subprocess.run(['cp', model_path, destination_path])
     print(f'Modèle copié pour {repo_name}')
 
 
